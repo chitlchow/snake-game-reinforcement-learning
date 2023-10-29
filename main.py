702e07c7c4c9396a3fa9686b5573b01433a084c8
Chit Lun Chow <chitlchow@gmail.com>
2023-10-29 23:29:23 +0000

    Added comment for main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.game import SnakeGame
-from src.agent import RLAgent
+from src.agent import QLAgent
 
 import pandas as pd
 import numpy as np
@@ -16,43 +16,62 @@ for f_name in os.listdir(pickles_dir):
         os.remove(f_path)
 
 def main():
+    # List for storing the result
     results = []
+
+    # Environment settings
+    # End episode if the agent earning nothing in 1000 stepsd
     steps_timeout = 1000
+    # Total number of episodes training
     num_episodes = 10000
+    # Target epsilon in the final episode
     final_epsilon = 0.001
-    agent = RLAgent(alpha=0.01, gamma=0.99, initial_epsilon=1.0)
+
+    # Initialize Q-learning agent
+    agent = QLAgent(alpha=0.01, gamma=0.99, initial_epsilon=1.0)
 
     agent.epsilon_decay = np.power(final_epsilon/agent.epsilon, 1/num_episodes)
     print(f"Using epsilon decay: {agent.epsilon_decay}")
-    game = SnakeGame(20, 20, enable_graphics=False)
+    game = SnakeGame(10, 10, enable_graphics=False)
     highest_score = 0
     for ep in range(num_episodes):
+        # Start the game
         game.start()
         steps = 0
         steps_without_score = 0
+
+        # Game loop
         while game.running:
+
             steps += 1
             if steps_without_score < steps_timeout:
                 if game.enable_graphics:
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                             game.reset()
+
                 # Retrieve the current state of the game
-
                 current_state = game.get_states()
+                # Agent make decision
                 action = agent.decision(current_state)
+                # Deploy action
                 game.snake.turn(action)
-
+                # Check reward given
                 reward = game.step()
+
                 if reward == 0:
                     steps_without_score += 1
-                else:
+                elif reward == 1:
                     # reset the timeout counter
                     steps_without_score = 0
+                else:
+                    steps_without_score += 1
+                    end_reason = "crash"
                 new_state = game.get_states()
                 agent.update_q_value(current_state, new_state, reward, action)
             else:
                 game.running = False
+                end_reason = "timeout"
         # When there are new records
         if game.score > highest_score:
             highest_score = game.score
@@ -64,16 +83,20 @@ def main():
                 }
                 pickle.dump(params, f)
 
-
-        result = {"episode": ep, "score": game.score, "epsilon": agent.epsilon, "steps": steps}
+        result = {"episode": ep+1, "score": game.score, "epsilon": agent.epsilon, "steps": steps, "alpha": agent.alpha, "end_reason": end_reason}
         print(result)
+
+        # perform epsilon decay
         agent.update_epsilon()
+        # Reset game
         game.reset()
+        # Store the result
         results.append(result)
+
+    # Create a dataframe for result analysis
     result_df = pd.DataFrame(results)
+    # Save the DF to result folder
     result_df.to_csv("result/q-learning-result.csv", index=None)
-
-
 
 if __name__ == "__main__":
     main()
